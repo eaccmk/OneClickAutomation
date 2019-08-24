42e540532abc2cd354229b57f5867a4f020c2196
Millan <eaccmk@gmail.com>
2019-08-24 17:12:26 +1000

    Added few interesting methods for automation :+1: :v:

--- src/test/java/com/test/CommanLibrary/Element.java
+++ src/test/java/com/test/CommanLibrary/Element.java
@@ -3,6 +3,9 @@ package com.test.CommanLibrary;
 import com.test.Configurations.DeviceSetUp;
 import org.openqa.selenium.By;
 import io.appium.java_client.MobileElement;
+import org.openqa.selenium.ElementNotVisibleException;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class Element {
 
@@ -13,8 +16,24 @@ public class Element {
 
     public boolean isElementDisplayed(By locator){
         MobileElement element = DeviceSetUp.driver.findElement(locator);
-        return element.isDisplayed();
+        try{
+            if(element.isDisplayed())
+                return element.isDisplayed();
+            }catch (ElementNotVisibleException ex) {
+                 return false;
+            }
+        return false;
     }
 
-    //TODO wait method
+    public void waitAndClickElement(By locator){
+        WebDriverWait wait = new WebDriverWait(DeviceSetUp.driver, 15); //wait set for 15 seconds for this call {Explicit wait}
+        MobileElement element = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(locator));
+        element.click();
+    }
+
+    public void enterText(By locator,String textValue){
+        WebDriverWait wait = new WebDriverWait(DeviceSetUp.driver, 15); //wait set for 15 seconds for this call {Explicit wait}
+        MobileElement element = (MobileElement) wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+        element.sendKeys(textValue);
+    }
 }

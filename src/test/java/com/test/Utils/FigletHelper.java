d8997b9900ca3e6be43abab086b3a2367b6b52a4
Millan <eaccmk@gmail.com>
2019-08-24 13:52:15 +1000

    This util will help you to print figlet way

--- src/test/java/com/test/Utils/FigletHelper.java
+++ src/test/java/com/test/Utils/FigletHelper.java
@@ -1,8 +1,19 @@
+/**
+ * @author: https://github.com/eaccmk
+ * */
+
 package com.test.Utils;
 
+import com.github.lalyos.jfiglet.FigletFont;
 
-import java.util.List;
-
-public class JavaUtils {
+public class FigletHelper {
+    public void figletPrint(String text) {
+        String asciiArt = null;
+        try {
+            asciiArt = FigletFont.convertOneLine(text);
+        } catch (Exception e) {
+            e.getCause().getMessage();
+        }
     }
+
 }

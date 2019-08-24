bd33d93beaeca685e4ee8691c85f4ab682451561
Millan <eaccmk@gmail.com>
2019-08-24 13:57:18 +1000

    All Java methods will sit in 8) class

--- src/test/java/com/test/Utils/JavaUtils.java
+++ src/test/java/com/test/Utils/JavaUtils.java
@@ -1,36 +1,23 @@
-package com.test.stepdefinitions;
+/**
+ * @author: https://github.com/eaccmk
+ * */
 
-import com.test.Configurations.FlickrAPIProcessor;
-import com.test.screens.SearchScreen;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import org.junit.Assert;
+package com.test.Utils;
 
-import java.util.List;
 
-public class SearchSteps {
+public class JavaUtils {
 
-    SearchScreen searchScreen;
-    FlickrAPIProcessor flickrAPIProcessor;
-
-    @Given("^User is on Search Screen$")
-    public void userIsOnSearchScreen() throws Throwable {
-        searchScreen = new SearchScreen();
-        searchScreen.waitForSearchScreen();
+    public void printThis(String stringToBePrinted){
+        System.out.println("\n"+stringToBePrinted+"\n");
     }
 
-    @When("^User Search for a text \"([^\"]*)\" in Search Screen$")
-    public void userSearchForATextInSearchScreen(String searchText) throws Throwable {
-        searchScreen = new SearchScreen();
-        searchScreen.searchForImagesByKeyword(searchText);
-    }
-
-    @Then("^Search results should display for search criteria \"([^\"]*)\"$")
-    public void searchResultsShouldDisplayForSearchCriteria(String searchText) throws Throwable {
-        searchScreen = new SearchScreen();
-        flickrAPIProcessor = new FlickrAPIProcessor();
-        List<String> searchResponse = flickrAPIProcessor.getSearchResponseTitles(searchText);
-        Assert.assertTrue(searchScreen.getSearchResults(searchText).equals(searchResponse));
+    public void letMeSleepForSeconds(int secondsSleepValue){
+        int milliSeconds = secondsSleepValue/100;
+        try{
+            Thread.sleep(milliSeconds);
+        }catch(Exception e){
+            printThis("Got Exception while waiting in letMeSleepForSeconds() ");
+        }
+        System.out.println("\n Sleeping for "+secondsSleepValue+" Seconds .. \n");
     }
 }
